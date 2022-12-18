d1b17696ce7324e6303e37aa0eb226e41b460185
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 14:57:34 +0500

    zadach27

---
+++ 4homework27/Program.cs
@@ -0,0 +1,13 @@
+﻿int line = int.Parse(Console.ReadLine());
+Console.WriteLine($"{line}->{func(line)}");
+
+int func(int numbers){
+    int summ = 0;
+    while (numbers > 0)
+    {
+        summ = summ + numbers % 10;
+        numbers /= 10;
+    }
+    return summ;
+}
+
