77d6c1793bb79ba591247e6a970eab878aaaad2a
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 14:52:40 +0500

    zadach25

---
+++ 4homework25/Program.cs
@@ -0,0 +1,13 @@
+﻿int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"{A},{B}->{multi(A, B)}");
+
+int multi(int ferst, int second){
+    int summ = 1;
+    for (int i = 1; i <= second; i++)
+    {
+        summ *= ferst;
+    }
+    return summ;
+}
+
