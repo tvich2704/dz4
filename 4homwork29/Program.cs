b6ae7972328d7b441efd1eac57d2e19e4332f759
krasnobaev.a.a <tvich2704@gmail.com>
2022-12-18 15:22:15 +0500

    dz3

---
+++ 4homwork29/Program.cs
@@ -0,0 +1,24 @@
+﻿rea(mass());
+
+ int[] mass(){
+    int[] array = new int[8];
+    for (int i = 0; i <= 7; i++)
+    {
+        array[i] = new Random().Next(101);
+    }
+    return array;
+ }
+
+void rea(int[] sub){
+    for(int i = 0; i <= sub.Length - 1; i++)
+    {
+        Console.Write($"{sub[i]} ");
+    }
+    Console.Write("->[");
+    for(int i = 0; i <= sub.Length - 1; i++)
+    {
+        Console.Write($"{sub[i]} ");
+    }
+    Console.Write("]");
+}
+
